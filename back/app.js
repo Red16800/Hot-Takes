9f9a3fedb741c6f60800821669536a43c7977ce9
Redouane <redouanekelai@gmail.com>
2022-05-09 12:48:45 +0200

    Final version

--- back/app.js
+++ back/app.js
@@ -5,9 +5,14 @@ const path = require('path');
 
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
+const limiter = require('express-rate-limit');
+require('dotenv').config();
+const limit = limiter({
+  windowMs: 10*60*1000,
+  max: 10
+})
 
-
-mongoose.connect(process.env.constDb,
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/test?retryWrites=true&w=majority`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -28,7 +33,7 @@ app.use(bodyParser.json());
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/sauces', sauceRoutes);
-app.use('/api/auth', userRoutes);
+app.use('/api/auth', limit, userRoutes);
 
 
 module.exports = app;
